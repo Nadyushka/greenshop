8084dbd83024ff7e85ecdf488b596893db095970
Nadyushka <12nadya11@gmail.com>
2024-01-29 16:47:59 +0300

    fix: set correct condition for NButton

--- components/ui/NButton.vue
+++ components/ui/NButton.vue
@@ -8,7 +8,7 @@ interface PropsType {
   btnTitle: string
 }
 
-const {leftIcon , rightIcon, btnTitle} = defineProps<PropsType>()
+const { leftIcon , rightIcon, btnTitle } = defineProps<PropsType>()
 
 defineEmits<{ (emit: 'btnClick'): void }>()
 
@@ -22,17 +22,17 @@ defineEmits<{ (emit: 'btnClick'): void }>()
     <LoginLogout
         v-if="leftIcon"
         :style="{
-          transform: this.leftIcon === 'logout' ? 'scaleX(-1)' : 'none'
+          transform: leftIcon === 'logout' ? 'scaleX(-1)' : 'none'
         }"
         class="button__left-icon"
-        filled="none"/>
+        :filled="false"/>
 
     {{ btnTitle }}
 
     <ArrowIcon
         v-if="rightIcon === 'arrow'"
         class="button__right-icon"
-        filled="none"
+        :filled="false"
     />
 
   </button>
